bcfea83822ec14404cb7e95d219cc28307f05c3a
Maria_Nadeem-10x <maria.nadeem@10xengineers.ai>
2022-07-24 18:27:31 +0500

    point out doctests with comment.

--- test.py
+++ test.py
@@ -3,7 +3,7 @@
 def couple(s, t):
     """Return a list of two-element lists in which the i-th element is [s[i], t[i]].
 
-    >>> a = [1, 2, 3]
+    >>> a = [1, 2, 3]            # doctests
     >>> b = [4, 5, 6]
     >>> couple(a, b)
     [[1, 4], [2, 5], [3, 6]]
